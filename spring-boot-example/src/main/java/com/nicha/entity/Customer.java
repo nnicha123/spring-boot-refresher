4b89f0226acd098e34b053f613cb76b99ea5263f
nnicha123 <nicha_ngamtweerat@hotmail.com>
2024-12-18 15:52:09 +0700

    Added lombok dependency and used @Data from lombok in Customer entity

--- spring-boot-example/src/main/java/com/nicha/entity/Customer.java
+++ spring-boot-example/src/main/java/com/nicha/entity/Customer.java
@@ -1,10 +1,11 @@
 package com.nicha.entity;
 
-import java.util.Objects;
 
 import jakarta.persistence.*;
+import lombok.Data;
 
 @Entity
+@Data
 public class Customer {
 	@Id
 	@SequenceGenerator(name = "customer_id_sequence", sequenceName = "customer_id_sequence", allocationSize = 1)
@@ -13,72 +14,6 @@ public class Customer {
 	private String name;
 	private String email;
 	private Integer age;
-	
-	public Customer(Integer id, String name, String email, Integer age) {
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.age = age;
-	}
-
-	public Customer() {
-	}
-
-	public Integer getId() {
-		return id;
-	}
-
-	public void setId(Integer id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(age, email, id, name);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Customer other = (Customer) obj;
-		return Objects.equals(age, other.age) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
-				&& Objects.equals(name, other.name);
-	}
-
-	@Override
-	public String toString() {
-		return "Customer [id=" + id + ", name=" + name + ", email=" + email + ", age=" + age + "]";
-	}
-	
 	
 	
 }
